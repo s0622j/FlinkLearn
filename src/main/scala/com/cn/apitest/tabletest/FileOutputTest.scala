a2000a88a4b640a21a2da810875244496edacff3
shengjie <598759654@qq.com>
2021-05-20 23:07:40 +0800

    flink学习7

--- src/main/scala/com/cn/apitest/tabletest/FileOutputTest.scala
+++ src/main/scala/com/cn/apitest/tabletest/FileOutputTest.scala
@@ -22,17 +22,43 @@ object FileOutputTest {
       .withSchema( new Schema()
         .field("id", DataTypes.STRING())
         .field("timestamp", DataTypes.BIGINT())
-        .field("temperature", DataTypes.DOUBLE())
+        .field("temp", DataTypes.DOUBLE())
       )
       .createTemporaryTable("inputTable")
 
     // 3.转换操作
     val sensorTable = tableEnv.from("inputTable")
-    val resultTable
+    // 3.1 简单转换
+    val resultTable = sensorTable
+      .select('id, 'temp)
+      .filter('id === "sensor_1")
+
+    // 3.2 聚合转换
+    val aggTable = sensorTable
+      .groupBy('id)  // 基于id分组
+      .select('id, 'id.count as 'count)
 
 
+    // 4. 输出到文件
+    // 注册输出表
+    val outputPath = "D:\\FlinkLearn\\src\\main\\resources\\outputsensor.txt"
+    tableEnv.connect( new FileSystem().path(outputPath) )
+      //      .withFormat( new OldCsv() )
+      .withFormat( new Csv() )
+      .withSchema( new Schema()
+        .field("id", DataTypes.STRING())
+//        .field("cnt", DataTypes.BIGINT())
+        .field("temp2333", DataTypes.DOUBLE())
+      )
+      .createTemporaryTable("outputTable")
 
+    resultTable.insertInto("outputTable")
+//    aggTable.insertInto("outputTable")  // 文件系统不支持
 
+//    resultTable.toAppendStream[(String, Double)].print("result")
+//    aggTable.toRetractStream[(String, Long)].print("agg")
+
+    env.execute("FileOutputTest")
 
   }
 
